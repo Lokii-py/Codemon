6890215a31500e6dacb24259345219bf6210d533
Lokesh Das <lokeshdas@Lokeshs-MacBook-Air.local>
2025-05-01 02:22:02 -0500

    Arena Removed from here and created it's own header

--- src/utility.cpp
+++ src/utility.cpp
@@ -5,57 +5,6 @@
 #include <ctime>
 using namespace std;
 
-void Arena::generateTerrain() {
-    // Fill terrainMap with valid terrain
-    char terrainTypes[] = { 'F', 'M', 'P', 'W', 'S' }; // 'F' = Forest, 'M' = Mountain, 'P' = Plain, 'W' = Water, 'S' = Swamp
-    // Initialize terrain grid
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 5; j++) {
-            // Randomly pick a terrain type for this cell
-            char newType;
-            do {
-                newType = terrainTypes[rand() % 5]; // Random terrain type
-            } while (hasSameTypeNeighbor(i, j, newType)); // Ensure no neighbor has the same type
-
-            terrainMap[i][j] = newType;
-        }
-    }
-};
-
-bool Arena::hasSameTypeNeighbor(int x, int y, char type) const {
-    // Check for neighbors (top, bottom, left, right) to ensure they are not the same type
-    // Top
-    if (x > 0 && terrainMap[x - 1][y] == type) {
-        return true;
-    }
-    // Bottom
-    if (x < 4 && terrainMap[x + 1][y] == type) {
-        return true;
-    }
-    // Left
-    if (y > 0 && terrainMap[x][y - 1] == type) {
-        return true;
-    }
-    // Right
-    if (y < 4 && terrainMap[x][y + 1] == type) {
-        return true;
-    }
-
-    return false;
-};
-
-void Arena::printTerrainForDebug() const {
-	// Print the terrain map for debugging
-	std::cout << "Terrain Map:\n";
-	for (int i = 0; i < 5; ++i) {
-		for (int j = 0; j < 5; ++j) {
-			std::cout << terrainMap[i][j] << " ";
-		}
-		std::cout << "\n";
-	}
-};
-
-
 const int POOL_SIZE = 12;
 
 const std::string CODEMON_NAMES[POOL_SIZE] = {
--- src/utility.h
+++ src/utility.h
@@ -18,30 +18,7 @@ public:
 	// checks if tile has same type neighbor
 	bool hasSameTypeNeighbor(int x, int y, char type) const;
 
-	//Desc: This function will deploy the codemon to the unoccupied and valid grid in arena
-	//Pre: takes a codemon represented as @
-	//Post: randomly deploys codemon to the unoccupied and valid grid in arena randomly
-	/* Example output: 
-		# # # # #
-		# M P M #
-		# P @ F #
-		# S S E # 
-		# # # # # 
-	========= */
-	void placeCodemon(char @, int x, int y);
-
-	void printTerrainForDebug() const;
-
-
-	//char getTile(int x, int y) const;
-	void printTerrainForDebug() const;
-
-
 	//bool isTileValid(int x, int y) const;
-
-	//void placeCodemon(Contestant& contestant); // places codemon in the arena
-	//void executeTurn(Contestant& active, Contestant& opponent); // executes turn for active contestant
-	//printVisibleMap(const Contestant& contestant); // prints visible map for the contestant
 };
 
 
