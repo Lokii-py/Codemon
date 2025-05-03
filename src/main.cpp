ddf642e5fcd241742ca2c6aaeabd96062bc12e69
henrycampb-29 <henrymorgancampbell@gmail.com>
2025-05-03 12:00:40 -0500

    updated main in regard to other file edits

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,6 @@
 // does a skill search. This new addition to HW 6 is 
 #include "Arena.h"
 #include "contestant.h"
-#include "Constant.h"
 #include "mechanic.h"
 #include <iostream>
 using namespace std;
@@ -15,15 +14,14 @@ using namespace std;
 int main() {
 
     Arena arena; //Initalize the battle arena
-    Contestant Player("Player"), Computer("CPU"); //initalize the contestant
-    BattleGround battle;
+    Contestant player("Player"), computer("CPU"); //initalize the contestant
 
     // Set random seed ONCE
     srand(1025);
 
     arena.generateTerrain(); // Fill terrainMap with valid terrain
-	arena.printTerrainForDebug();
-    
+    arena.printTerrainForDebug();
+
     char choice;
     std::cout << "PLAYER: Do you want custom selection of Codemons? (y/n)\n";
     std::cin >> choice;   // user selects or random
@@ -35,45 +33,66 @@ int main() {
     }
     computer.generateCodemons();
 
+    //choose codemons
+
+
     //Deploy codemon to the unoccupied and valid grid in arena randomly
     int row, col;
-    do{
+    do {
         row = rand() % 5;
         col = rand() % 5;
-    }  while (arena.isOccupied(row, col));
-    
+    } while (arena.isOccupied(row, col));
+    //place codemon
+    arena.setTerrainTile(row, col, '@');
+    arena.markOccupied(row, col);
+
+    // place enemy
+    do {
+        row = rand() % 5;
+        col = rand() % 5;
+    } while (arena.isOccupied(row, col));
+    //place enemy
+    arena.setTerrainTile(row, col, 'E');
+    arena.markOccupied(row, col);
+
+
+    //print visible map
+    arena.printVisibleMap();
+
+
     char move;
     cout << "Please choose the place to position your codemon: ";
     cin >> move;
-    
+
     movement(move, row, col, arena);
     //row and col afterward this place is the position you can use it to specify the codemon position
     // TO DO: Put codemon position in the arena grid displaying 3 *3 grid .
-    
+
     //Movement for computer:
     bool Random = true;
     if (Random) {
         int num = rand() % 8;
-        char moves[] = {'W', 'A', 'S', 'D', 'Q', 'E', 'Z', 'C'};
-        movement(moves[num], row, col, arena, true);
+        char moves[] = { 'W', 'A', 'S', 'D', 'Q', 'E', 'Z', 'C' };
+        movement(moves[num], row, col, arena);
     }
 
-    if(Player && battle.isEnemyInRange(row, col, arena)){
-        cout<< "Enemy in range! Initiating battle..." << endl;
+    if (isEnemyInRange(row, col, arena)) {
+        cout << "Enemy in range! Initiating battle..." << endl;
         string response, skill;
         cout << "Do you want to attack? (yes/no)";
         cin >> response;
         if (response == "yes") {
             cout << "Select a skill to attack with: ";
             cin >> skill;
-        } else {
+        }
+        else {
             cout << "Not attacking." << endl;
         }
     }
 
-    if(Computer && battle.isEnemyInRange(row, col, arena)){
+    if (isEnemyInRange(row, col, arena)) {
         // start attacter
     }
-    
+
     return 0;
-}
+} 
