1b5859942a7747dbe10379aefe594bcdcdbb340e
henrycampb-29 <henrymorgancampbell@gmail.com>
2025-05-03 11:53:47 -0500

    updated arena visibility functions

--- src/Arena.cpp
+++ src/Arena.cpp
@@ -1,4 +1,19 @@
-#include "utility.h"
+#include "Arena.h"
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+Arena::Arena() {
+    for (int r = 0; r < SIZE; ++r) {
+        for (int c = 0; c < SIZE; ++c) {
+            occupied[r][c] = false;
+            visibleMap[r][c] = '#';
+        }
+    }
+    generateTerrain();
+    curRow = -1; // not placed yet
+    curRow = -1;
+}
 
 void Arena::generateTerrain() {
     // Fill terrainMap with valid terrain
@@ -17,15 +32,15 @@ void Arena::generateTerrain() {
     }
 }
 
-bool Arena::isOccupied(int row, int col) const{
+bool Arena::isOccupied(int row, int col) const {
     return occupied[row][col];
 }
 
-void Arena::markOccupied(int row, int col){
+void Arena::markOccupied(int row, int col) {
     occupied[row][col] = true;
 }
 
-char Arena::getTerrainTile(int row, int col) const{
+char Arena::getTerrainTile(int row, int col) const {
     char type;
     type = terrainMap[row][col];
     return type;
@@ -51,59 +66,52 @@ bool Arena::hasSameTypeNeighbor(int x, int y, char type) const {
     }
 
     return false;
-}
+};
 
 void Arena::printTerrainForDebug() const {
-	// Print the terrain map for debugging
-	std::cout << "Terrain Map:\n";
-	for (int i = 0; i < 5; ++i) {
-		for (int j = 0; j < 5; ++j) {
-		    cout << terrainMap[i][j] << " ";
-		}
-		std::cout << "\n";
-	}
+    // Print the terrain map for debugging
+    cout << "Terrain Map:\n";
+    for (int i = 0; i < 5; ++i) {
+        for (int j = 0; j < 5; ++j) {
+            cout << terrainMap[i][j] << " ";
+        }
+        cout << "\n";
+    }
 }
 
 
+void Arena::updateVisibility(int x, int y) {
+    for (int i = 0; i < SIZE; ++i) {
+        for (int j = 0; j < SIZE; ++j) {
+            visibleMap[i][j] = '#';
+        }
+    }
+    for (int i = -1; i <= 1; ++i) {
+        for (int j = -1; j <= 1; ++j) {
+            int nx = x + i;
+            int ny = y + j;
+            if (nx >= 0 && nx < SIZE && ny >= 0 && ny < SIZE)
+                visibleMap[nx][ny] = terrainMap[nx][ny];
+        }
+    }
+}
 
+void Arena::setTerrainTile(const int n_row, const int n_col, const char c) {
+    if (curRow != -1 && curCol != -1) {
+        visibleMap[curRow][curCol] = terrainMap[curRow][curCol]; // replaces old spot with terain symbol if not first move
+    }
+    visibleMap[n_row][n_col] = c;
+    return;
+}
 
-const int POOL_SIZE = 12;
-
-const std::string CODEMON_NAMES[POOL_SIZE] = {
-    "Flareth", // Fire
-    "Aquagon", // Water
-    "Leafowl", // Grass 
-    "Voltiguana", // Electric 
-    "Infermouse", // Fire
-    "Bubbletail", // Water
-    "Shrubling", // Grass 
-    "Zapkit", // Electric
-    "Cindermole", // Fire
-    "Torrentoise", // Water 
-    "Thornabbit", // Grass
-    "Shockpup" // Electric
-};
-
-const int CODEMON_HP[POOL_SIZE] = {
-    40, 42, 38, 35,
-    34, 45, 37, 32,
-    39, 48, 40, 36
-};
-
-const int CODEMON_LEVELS[POOL_SIZE] = {
-    5, 4, 2, 6,
-    7, 3, 1, 0,
-    8, 9, 10, 11
-};
-
-const std::string CODEMON_SKILLS[POOL_SIZE] = {
-    "Flame Burst", "Aqua Jet", "Razor Leaf", "Thunder Jolt",
-    "Fire Claw", "Bubble Beam", "Vine Whip", "Static Bolt",
-    "Ember Rage", "Water Pulse", "Spore Shot", "Spark Bite"
-};
-
-const int CODEMON_SKILL_DAMAGE[POOL_SIZE] = {
-    12, 10, 11, 13,
-    15, 14, 19, 17,
-    16, 18, 20, 21
-};
+void Arena::printVisibleMap() const {  // I know it is not "void Contestant::printVisibleMap() const"
+    // Print the visible map
+    for (int i = 0; i < 5; ++i) {
+        for (int j = 0; j < 5; ++j) {
+            cout << visibleMap[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    std::cout << "==========\n";
+}
+ 
--- src/Arena.h
+++ src/Arena.h
@@ -1,20 +1,25 @@
 #ifndef ARENA_H
 #define ARENA_H
 
+#include <iostream>
+
 class Arena {
 private:
     static const int SIZE = 5;
-	char terrainMap[SIZE][SIZE];
+    char terrainMap[SIZE][SIZE]; // 2D array for terrain
     bool occupied[SIZE][SIZE]{};
-
+    char visibleMap[SIZE][SIZE];
+    int curRow;
+    int curCol;
+    
 public:
 
     Arena();
 
-	//Desc: This function will generate the terrain map for he arena
-	//pre: None
-	//post: Generates a 5 * 5 grid of terrain types
-	void generateTerrain();
+    //Desc: This function will generate the terrain map for he arena
+    //pre: None
+    //post: Generates a 5 * 5 grid of terrain types
+    void generateTerrain();
 
     //Desc: This function will generate True if codemon is already there
     //Pre: None
@@ -26,23 +31,28 @@ public:
     //Post: None
     void markOccupied(int row, int col);
 
-    //Desc: This setter function will set the grid of the terrain map as per the char
-    //Pre: row and col are passed as the const
-    //Post: None update the terrain map
-    void setTerrainTile(const int row, const int col, char c);
-
     //Desc: This function will return back the terrain tile
     //Pre: Row and col are integers
     //Post: Return 'F' = Forest, 'M' = Mountain, 'P' = Plain, 'W' = Water, 'S' = Swamp
     char getTerrainTile(int row, int col) const;
 
-	//Desc: checks if tile has same type neighbor
+    //Desc: checks if tile has same type neighbor
     //Pre: x, t are integer and type is char
     //Post: Check for neighbors (top, bottom, left right) to ensure they are not same type
-	bool hasSameTypeNeighbor(int x, int y, char type) const;
+    bool hasSameTypeNeighbor(int x, int y, char type) const;
 
     //for debugging
-	void printTerrainForDebug() const;
+    void printTerrainForDebug() const;
+
+    
+    void setTerrainTile(const int n_row, const int n_col, const char c);
+    
+    
+    void updateVisibility(int x, int y);
+    
+    void printVisibleMap() const;
+
+
 };
 
 #endif
