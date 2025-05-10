1060f9133341028538686490b2fd6772e3781582
Lokesh Das <lokeshdas@Lokeshs-MacBook-Air.local>
2025-05-09 21:53:27 -0500

    Updated item class

--- src/item.cpp
+++ src/item.cpp
@@ -1,58 +1,26 @@
 #include "item.h"
+#include <iostream>
+using namespace std;
 
-// Public functions 
-// Default constructor
 Item::Item() {
     name = "Unnamed";
     description = "None";
     effectType = "None";
-    duration = 0;
+    effectAmount = 0;
 }
 
 // Parameterized constructor
-Item(const std::string& n, std::string& type, int dur, const std::string& des){
+Item::Item(const std::string& n, std::string& type, int dur, const std::string& des){
     name = n;
     effectType = type;
-    duration = dur;
+    effectAmount = dur;
     description = des;
 }
 
-int Item::getDuration() const {
-    return duration;
+int Item::getEffectAmount() const {
+    return effectAmount;
 }
 
 void Item::print() const {
-    std::cout << "  - " << name << " - " << description << " (Duration: " << duration << ")\n";
+    std::cout << "  - " << name << " - " << description << " (Effect Amount: " << effectAmount << ")\n";
 }
-
-// void loadItemsFromFile(const string& filename, Item itemArray[], int maxSize) {
-//     ifstream fin(filename); // Open the file for reading
-//     if (!fin) {
-//         cerr << "Error: Could not open file " << filename << endl;
-//         exit(1); // Exit the program if the file can't be opened
-//     }
-
-//     int count = 0;
-
-//     // Read up to maxSize items from the file
-//     while (count < maxSize) {
-//         string name, type, amountStr, description;
-
-//         // Read each field using comma as the delimiter
-//         getline(fin, name, ',');
-//         getline(fin, type, ',');
-//         getline(fin, amountStr, ',');
-//         getline(fin, description); // Reads the remainder of the line (until newline)
-
-//         // Convert amount string to integer
-//         int amount = stoi(amountStr);
-
-//         // Create and store the Item in the array
-//         itemArray[count] = Item(name, description, type, amount);
-//         count++;
-//     }
-
-//     fin.close(); // Close the file 
-// }
-
-
--- src/item.h
+++ src/item.h
@@ -1,44 +1,25 @@
 #ifndef ITEM_H
 #define ITEM_H
-
-#include <string>
 #include <iostream>
-#include <fstream> 
 using namespace std;
 
 class Item {
 private:
-    std::string name;
-    std::string description;
-    string effectType; // "Heal", "Boost", or "Shield"
-    int duration;
-
+	string name;
+	string description;
+	string effectType; // "Heal", "Boost", or "Shield"
+	int effectAmount;
 public:
-    // Desc: This default constructor sets the inital values for the item
-    // Pre: None
-    // Post: sets name to "Unnamed", description to "None", and duration to 0.
-    Item();
+	// Public functions
+	// Default constructor
+	Item();
 
-    // Desc: This is a constructor that allows for the inputs of the item to be passed in as value.
-    // Pre: need string arguements and int arguement
-    // Post: Takes argument for the name, description, and duration and assigns them accordingly. 
-    Item(const std::string& n, std::string& type, int dur, const std::string& des);
+	// Parameterized constructor
+	Item(const std::string& n, std::string& type, int dur, const std::string& des);
 
-    // Desc: This functions returns the duration of the item
-    // Pre: none
-    // Post: returns int of duration of item
-    int getDuration() const;
+	int getEffectAmount() const;
 
-    // Desc: This functions prints out the items nicely
-    // Pre: None
-    // Post: formats and print line for each item
-    void print() const;
-    
-    // Desc: This function loads items from a file into an array of Item objects
-    // Pre: filename is the name of the file to read from, itemArray is the array to store items, maxSize is the maximum number of items
-    // Post: Reads items from the file and populates the itemArray with Item objects
-    // friend void loadItemsFromFile(const string& filename, Item itemArray[], int maxSize);
-    //friend class Codemon;
+	void print() const;
 };
 
 #endif
