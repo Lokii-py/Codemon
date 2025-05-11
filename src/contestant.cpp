19fb12f1433a3a7d7288c338f46aaaaa05b8e6fb
henrycampb-29 <henrymorgancampbell@gmail.com>
2025-05-10 21:05:59 -0500

    updated my branch and added comments

--- src/contestant.cpp
+++ src/contestant.cpp
@@ -1,211 +1,201 @@
 #include "contestant.h"
+#include "item.h"
+#include "Arena.h"
+#include "Codemon.h"
+#include "mechanic.h"
 #include <iostream>
 #include <string>
 using namespace std;
 
-// Member Function Definition
-// Default constructor
+void Contestant::resizeQueue(int newCapacity) {
+	Item* newQueue = new Item[newCapacity];
+	for (int i = 0; i < qSize; i++) {
+		newQueue[i] = itemQueue[(qFront + i) % qCapacity];
+	}
+	delete[] itemQueue;
+	itemQueue = newQueue;
+	qCapacity = newCapacity;
+	qFront = 0;
+}
+
+void Contestant::placeRandomCodemon(Arena& A) {
+	do {
+		row = rand() % 12;
+		col = rand() % 12;
+	} while (A.isOccupied(row, col));
+	return;
+}
+
+void Contestant::setActiveCodemon(const int n) {
+	activeCodemon = n;
+}
+
 Contestant::Contestant() {
-    std::cout << "Enter contestant name: ";
-    std::getline(std::cin, name);
-    codemonCount = 0;
-    alive = true;
+	playerName = " ";
+	isHuman = true;
+	row = -1;
+	col = -1;
 }
 
-// Parameterized constructor
-Contestant::Contestant(std::string n) {
-    name = n;
-    codemonCount = 0;
-    alive = true;
+Contestant::Contestant(string name, bool human, int codemonNum) {
+	playerName = name;
+	isHuman = human;
+	setTeamSize(codemonNum);
+	activeCodemon = 0;
+	row = -1;
+	col = -1;
+	team = new Codemon[teamsize];
+	void placeRandomCodemon(Arena & A);
 }
 
-// Copy constructor
-Contestant::Contestant(const Contestant& clone) {
-    std::cout << "Cloning contestant: " << clone.name << "\n";
-    std::cout << "Enter name for cloned contestant: ";
-    std::getline(std::cin, name); // copy constructor only changes the name
-    codemonCount = clone.codemonCount;
-
-    for (int i = 0; i < codemonCount; ++i) // copies over all data
-        pocket[i] = clone.pocket[i];
-    alive = true;
+Contestant::~Contestant() {
+	delete[] team;
 }
 
-std::string Contestant::getName() const {
-    return name;
+Contestant::Contestant(const Contestant& other) {
+	cout << "Copying Contestant: " << other.playerName << endl;
+	cout << "Enter name for cloned Contestant: ";
+	getline(cin, playerName);
+
+	teamsize = other.teamsize;
+	activeCodemon = other.activeCodemon;
+	row = other.row;
+	col = other.col;
+	isHuman = other.isHuman;
+
+	team = new Codemon[teamsize];
+	for (int i = 0; i < teamsize; ++i) {
+		team[i] = other.team[i];
+	}
 }
 
-int Contestant::getCodemonCount() const {
-    return codemonCount;
+Contestant& Contestant::operator = (const Contestant& rhs) {
+	if (this == &rhs) {
+		return *this;
+	}
+
+	delete[] team;
+
+	this->playerName = rhs.playerName;
+	this->teamsize = rhs.teamsize;
+	this->activeCodemon = rhs.activeCodemon;
+	this->row = rhs.row;
+	this->col = rhs.col;
+	this->isHuman = rhs.isHuman;
+
+	team = new Codemon[teamsize];
+	for (int i = 0; i < teamsize; ++i) {
+		team[i] = rhs.team[i];
+	}
+	return *this;
 }
 
-
-void Contestant::updateCodemon(const Codemon& codemon) {
-    if (codemonCount >= 10) {
-        std::cout << "Pocket is full.\n"; // can't add more than 10 codemons
-        return;
-    }
-
-    for (int i = 0; i < codemonCount; i++) { // can't add codemon with same name
-        if (pocket[i].getName() == codemon.getName()) {
-            std::cout << "Codemon with the same name already exists. Can't add Codemon.\n";
-            return;
-        }
-    }
-
-    pocket[codemonCount++] = codemon;  // assigns codemon at index then increases count
-
-}
-
-void Contestant::selectCodemons() {
-    for (int i = 0; i < 3; i++) {
-        std::string codemonName, type;
-        int level, hp;
-
-        std::cin.ignore();
-        std::cout << "Enter Codemon #" << i + 1 << " name: "; // taking in parameters
-        std::getline(std::cin, codemonName);
-        std::cout << "Enter type: ";
-        std::getline(std::cin, type);
-        std::cout << "Enter level: ";
-        std::cin >> level;
-        std::cout << "Enter HP: ";
-        std::cin >> hp;
-        std::cin.ignore();
-
-        Codemon codemon(codemonName, type, level, hp); // initializes codemon with values
-
-        // Add Skills to Codémon
-        for (int i = 0; i < 6; i++) {
-            char skillChoice;
-            std::cout << "Add a skill to " << codemonName << "? (y/n): ";
-            std::cin >> skillChoice;
-            if (skillChoice != 'y') {
-                break;
-            }
-            std::string skillName;
-            int damage;
-
-            std::cin.ignore();
-            std::cout << "  Enter skill name: "; // taking in skills info
-            std::getline(std::cin, skillName);
-            std::cout << "  Enter base damage: ";
-            std::cin >> damage;
-            std::cin.ignore();
-
-            Skill skill(skillName, damage); // intializes skill
-            codemon.addSkill(skill);        // then adds skill
-        }
-
-        updateCodemon(codemon); // adds codemon
-    }
-
-}
-
-const int POOL_SIZE = 12;
-
-const std::string CODEMON_NAMES[POOL_SIZE] = {
-    "Flareth", // Fire
-    "Aquagon", // Water
-    "Leafowl", // Grass 
-    "Voltiguana", // Electric 
-    "Infermouse", // Fire
-    "Bubbletail", // Water
-    "Shrubling", // Grass 
-    "Zapkit", // Electric
-    "Cindermole", // Fire
-    "Torrentoise", // Water 
-    "Thornabbit", // Grass
-    "Shockpup" // Electric
-};
-
-const std::string CODEMON_TYPES[4] = {
-    "Fire",
-    "Water",
-    "Grass",
-    "Electric",
-};
-
-const int CODEMON_HP[POOL_SIZE] = {
-    40, 42, 38, 35,
-    34, 45, 37, 32,
-    39, 48, 40, 36
-};
-
-const int CODEMON_LEVELS[POOL_SIZE] = {
-    5, 4, 2, 6,
-    7, 3, 1, 0,
-    8, 9, 10, 11
-};
-
-const std::string CODEMON_SKILLS[POOL_SIZE] = {
-    "Flame Burst", "Aqua Jet", "Razor Leaf", "Thunder Jolt",
-    "Fire Claw", "Bubble Beam", "Vine Whip", "Static Bolt",
-    "Ember Rage", "Water Pulse", "Spore Shot", "Spark Bite"
-};
-
-const int CODEMON_SKILL_DAMAGE[POOL_SIZE] = {
-    12, 10, 11, 13,
-    15, 14, 19, 17,
-    16, 18, 20, 21
-};
-
-void Contestant::generateCodemons() {
-    for (int i = 0; i < 3; i++) {
-        std::string codemonName, type;
-        int level, hp;
-        int random = rand() % 12;
-        codemonName = CODEMON_NAMES[random];
-        type = CODEMON_TYPES[random % 4];
-        level = CODEMON_LEVELS[rand() % 12];
-        hp = CODEMON_HP[rand() % 12];
-
-        Codemon codemon(codemonName, type, level, hp); // initializes codemon with values
-
-        // Add Skills to Codémon
-        std::string skillName;
-        int damage;
-
-        skillName = CODEMON_SKILLS[random];
-        damage = CODEMON_SKILL_DAMAGE[rand() % 12];
-
-        Skill skill(skillName, damage); // intializes skill
-        codemon.addSkill(skill);        // then adds skill
-
-
-        updateCodemon(codemon); // adds codemon
-
-    }
-
-}
-
-void Contestant::death() {
-    codemonCount -= 1;
-    if (codemonCount == 0) {
-        alive = false;
-    }
-    return;
-}
-
-bool Contestant::isAlive() {
-    return alive;
-}
-
-std::ostream& operator<<(std::ostream& os, const Codemon& c) {
-    os << "Name: " << c.getName() << "\n";
-    os << "Type: " << c.getType() << "\n";
-    os << "Level: " << c.getLevel() << "\n";
-    os << "HP: " << c.getCurrentHP() << "/" << c.getMaxHP() << "\n";
-    return os;
-}
-
-Codemon& Contestant::getActiveCodemon() {
-    if (codemonCount > 0) {
-        return pocket[codemonCount - 1];
-    }
-    else {
-        std::cout << "No active Codemon remaining.";
-    }
+void Contestant::initSharedQueue(int firstCap) {
+	delete[] itemQueue;
+	itemQueue = new Item[firstCap];
+	qCapacity = firstCap;
+	qSize = 0;
+	qFront = 0;
 }
 
 
+void Contestant::setTeamSize(int n) {
+	delete[] team;
+	teamsize = n;
+	activeCodemon = 0;
+	team = new Codemon[teamsize];
+}
+
+bool Contestant::addCodemon(const Codemon& c, int idx) {
+	if (idx < 0 || idx >= teamsize) return false;
+	team[idx] = c;
+	return true;
+}
+
+Codemon& Contestant::getActive() {
+	return team[activeCodemon];
+}
+
+void Contestant::spawn(Arena& arena, int r, int c, char symbol) {
+	row = r;
+	col = c;
+	arena.setTerrainTile(row, col, symbol);
+}
+
+void Contestant::takeTurn(Arena& arena, char symbol) {
+	if (isHuman) {
+		arena.printVisibleMap();
+		cout << "[" << playerName << "] move (W A S D Q E Z C): ";
+		char mv;
+		cin >> mv;
+		movement(mv, symbol, arena);
+	}
+	else {
+		const char dir[8] = { 'W','A','S','D','Q','E','Z','C' };
+		char mv = dir[rand() % 8];
+		movement(mv, symbol, arena);
+	}
+	row = arena.getCurRow();
+	col = arena.getCurCol();
+}
+
+void Contestant::enqueueItem(const Item& item) {
+	if (qSize == qCapacity) {
+		resizeQueue(qCapacity * 2);
+	}
+	int pos = (qFront + qSize) % qCapacity;
+	itemQueue[pos] = item;
+	qSize++;
+}
+bool Contestant::useNextItem() {
+	if (qSize == 0) return false;
+	const Item& item = itemQueue[qFront];
+	cout << "Using item: ";
+	item.print();
+	// Apply effect to active codemon if needed
+	qFront = (qFront + 1) % qCapacity;
+	qSize--;
+	if (qCapacity > 4 && qSize < qCapacity / 4) resizeQueue(qCapacity / 2);
+	return true;
+}
+
+void Contestant::printVisibleMap(const Arena& A) const {
+	cout << "=== " << playerName << "'s visible map ===\n";
+	A.printVisibleMap();
+}
+
+bool Contestant::isAlive() const {
+	return !(team[activeCodemon].isFainted());
+}
+
+void Contestant::selectCodemons(Codemon pool[], int poolSize, bool random, int n) {
+	setTeamSize(n);
+
+	if (random) {
+		for (int i = 0; i < n; i++)
+			team[i] = pool[rand() % poolSize];
+	}
+	else {
+		for (int i = 0; i < n; i++) {
+			cout << "\nPick Codemon #" << i + 1 << ":\n";
+			for (int j = 0; j < poolSize; ++j) {
+				cout << j << ": " << pool[j].getName() << " (" << pool[j].getType() << ")\n";
+			}
+			int choice;
+			cout << endl << "pick a number from the pool: ";
+			cin >> choice;
+			while (choice < 0 || choice >= poolSize) {
+				cout << "Invalid. Try again: ";
+				cin >> choice;
+			}
+			team[i] = pool[choice];
+		}
+	}
+}
+
+
+// Static member initialization
+Item* Contestant::itemQueue = nullptr;
+int   Contestant::qCapacity = 0;
+int   Contestant::qSize = 0;
+int   Contestant::qFront = 0;
--- src/contestant.h
+++ src/contestant.h
@@ -1,88 +1,195 @@
 #ifndef CONTESTANT_H
 #define CONTESTANT_H
-
+#include "Arena.h"
 #include "Codemon.h"
+#include "item.h"
 #include <string>
+using namespace std;
 
 class Contestant {
 private:
-    std::string name;
-    int codemonCount;
-    Codemon pocket[3];
-    Codemon currentCodemon;
-    bool alive;
+    Codemon* team = nullptr;
+    int teamsize = 0;
+    int activeCodemon = 0;
+
+    int row = -1;
+    int col = -1;
+
+    string playerName;
+    bool isHuman = true;
+
+    //shared dynamic Item Queueu
+    static Item* itemQueue;
+    static int qCapacity;
+    static int qSize;
+    static int qFront;
+
+    // Desc: Resizes the shared item queue
+        // Pre: newCapacity > current capacity
+        // Post: Resizes and copies old items to new array
+    void resizeQueue(int newCapacity);
 
 public:
-    // Constructors
-    // Desc: This is default constructor that asks only the contestant's name
+    // Desc: Default constructor
     // Pre: None
-    // Post: Assigns contestant’s name from input and sets the number of Codémons and items owned to 0. 
-    Contestant(); // Default constructor
+    // Post: Initializes contestant with defaults
+    Contestant();
 
-    // Desc: This is a constructor that allows for the input to be passed in as value.
-    // Pre: need string arguement
-    // Post: Takes argument for the name and sets the number of Codémons and items owned to 0. 
-    Contestant(std::string n); // paremeterized constructor
+    // Desc: Parameterized constructor that sets name, human status, and team size
+    // Pre: name is a valid string, codemonNum ≥ 0
+    // Post: Initializes player with name and sets up team
+    Contestant(string name, bool human, int codemonNum);
 
-    // Desc: This is a copy constructor that asks for a new name 
-    // Pre: need a contestant parameter
-    // Post: copies data then assigns new name form input
-    Contestant(const Contestant& clone);
-
-    //Functions
-    // Desc: This functions returns the name of the player
-    // Pre: none
-    // Post: returns string of contestant name
-    std::string getName() const;
-
-    // Desc: This function returns the number of Codémons the contestant has
+    // Desc: Destructor
     // Pre: None
-    // Post: Returns the value of codemonCount
-    int getCodemonCount() const;
+    // Post: Cleans up dynamic team memory
+    ~Contestant();
 
-    // Desc: This function updates a contestant's pocket by adding a new Codémon info. 
-    // Pre: need a codemon parameter
-    // Post: If pocket is not full and codemon does not already exist, it adds 
-    void updateCodemon(const Codemon&);
+    // Desc: Copy constructor
+    // Pre: other must be a valid Contestant
+    // Post: Deep copies other into this Contestant
+    Contestant(const Contestant& other);
 
-    // Desc: This function allows for the player to select their codemons
+    // Desc: Returns the contestant's name
+    std::string getName() const { return playerName; }
+
+    // Desc: Assignment operator overload
+    // Pre: rhs must be a valid Contestant
+    // Post: Assigns rhs's data into this Contestant
+    Contestant& operator = (const Contestant& rhs);
+
+    // Desc: Places all Codemons randomly on the map
+    // Pre: Arena must be initialized
+    // Post: Codemons placed in valid cells
+    void placeRandomCodemon(Arena& A);
+
+    // Desc: Sets active Codemon by index
+    // Pre: n must be a valid team index
+    // Post: Updates activeCodemon index
+    void setActiveCodemon(const int n);
+
+    // Desc: Initializes shared item queue with first capacity
+    // Pre: firstCap > 0
+    // Post: Allocates static queue
+    static void initSharedQueue(int firstCap);
+
+    // Desc: Sets the team size for the contestant
+    // Pre: n ≥ 0
+    // Post: Allocates dynamic team array
+    void setTeamSize(int n);
+
+    // Desc: Adds a Codemon to the team at specified index
+    // Pre: idx < team size
+    // Post: Adds Codemon at idx
+    bool addCodemon(const Codemon& c, int idx);
+
+    // Desc: Returns reference to currently active Codemon
+    // Pre: Team must be initialized
+    // Post: Returns Codemon& reference
+    Codemon& getActive();
+
+    // Desc: Spawns contestant at a position in arena
+    // Pre: r and c are valid coordinates
+    // Post: Updates row/col, places player on arena
+    void spawn(Arena& arena, int r, int c, char symbol);
+
+    // Desc: Takes a turn (move, attack, etc.)
+    // Pre: Arena must be initialized
+    // Post: Applies contestant's actions
+    void takeTurn(Arena& arena, char symbol);
+
+    // Desc: Enqueues an item to the shared queue
+    // Pre: item must be valid
+    // Post: Item added to the rear of queue
+    void enqueueItem(const Item& item);
+
+    // Desc: Uses the next item in queue
+    // Pre: Queue must not be empty
+    // Post: Applies effect to active Codemon
+    bool useNextItem();
+
+    // Desc: Selects team Codemons from pool (manual or random)
+    // Pre: pool must be valid, poolSize ≥ n
+    // Post: Team is populated with selected Codemons
+    void selectCodemons(Codemon pool[], int poolSize, bool random, int n);
+
+    // Desc: Prints a 3x3 visible portion of arena centered on the contestant
+    // Pre: Arena must be initialized
+    // Post: Outputs partial map view
+    void printVisibleMap(const Arena& A) const;
+
+    // Desc: Returns current row position
+    // Pre: row must be initialized
+    // Post: Returns int row
+    int getRow() const { return row; }
+
+    // Desc: Returns current column position
+    // Pre: col must be initialized
+    // Post: Returns int col
+    int getCol() const { return col; }
+
+    // Desc: Returns true if the player is human
     // Pre: none
-    // Post: asks player to creat three codemons and assigns them to their pocket
-    void selectCodemons();
+    // Post: Returns bool isHuman
+    bool isHum() const { return isHuman; }
 
-    // Desc: This function generates three random codemons
-    // Pre: none
-    // Post: generates and assignns random codemons
-    void generateCodemons();
+    // Desc: Returns true if any Codemon is still alive
+    // Pre: Team must be initialized
+    // Post: Returns true if at least one Codemon is alive
+    bool isAlive() const;
 
-    // Desc: This function kills the object and ends the game
-    // Pre: none
-    // Post: sets alive to false
-    void death();
+    // class Contestant {
+    // private:
+    //     std::string name;
+    //     int codemonCount;
+    //     Codemon pocket[3];
+    //     Codemon currentCodemon;
+    //     bool alive;
 
-    // Desc: This function returns the status of the player
-    // Pre: none
-    // Post: returns bool of isAlive
-    bool isAlive();
+    // public:
+    //     // Constructors
+    //     // Desc: This is default constructor that asks only the contestant's name
+    //     // Pre: None
+    //     // Post: Assigns contestants name from input and sets the number of Codemons and items owned to 0. 
+    //     Contestant(); // Default constructor
 
+    //     // Desc: This is a copy constructor that asks for a new name 
+    //     // Pre: need a contestant parameter
+    //     // Post: copies data then assigns new name form input
+    //     Contestant(const Contestant& clone);
 
-    // Desc: This function overloads the << operator to print
-    // Pre: need os and codemon parameter
-    // Post: prints a codemon stats
-    friend std::ostream& operator<<(std::ostream& os, const Codemon& c);
+    //     //Functions
+    //     
 
-    // Desc: This function returns the current codemon being used by the player
-    // Pre: none
-    // Post: returns Codemon type of active codemon
-    Codemon& getActiveCodemon();
+    //     // Desc: This function returns the number of Cod�mons the contestant has
+    //     // Pre: None
+    //     // Post: Returns the value of codemonCount
+    //     int getCodemonCount() const;
 
+    //     // Desc: This function updates a contestant's pocket by adding a new Codemon info. 
+    //     // Pre: need a codemon parameter
+    //     // Post: If pocket is not full and codemon does not already exist, it adds 
+    //     void updateCodemon(const Codemon&);
 
-    // Desc: This friend function runs the battle
-    // Pre: needs contestants and arena
-    // Post: returns nothing but edits contestants
-   // friend void battle(Contestant& player, Contestant& comp, Arena& arena);
+    //     // Desc: This function allows for the player to select their codemons
+    //     // Pre: none
+    //     // Post: asks player to creat three codemons and assigns them to their pocket
+    //     void selectCodemons();
 
+    //     // Desc: This function generates three random codemons
+    //     // Pre: none
+    //     // Post: generates and assignns random codemons
+    //     void generateCodemons();
+
+    //     // Desc: This function overloads the << operator to print
+    //     // Pre: need os and codemon parameter
+    //     // Post: prints a codemon stats
+    //     friend std::ostream& operator<<(std::ostream& os, const Codemon& c);
+
+    //     // Desc: This friend function runs the battle
+    //     // Pre: needs contestants and arena
+    //     // Post: returns nothing but edits contestants
+    //     friend void battle(Contestant& player, Contestant& comp, Arena& arena);
 };
 
 #endif
-
